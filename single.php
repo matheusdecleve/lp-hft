9aa9d60711cd74f5d8731871bb96cd7a69c3ec38
Matheus Decleve <me@matheus.in>
2024-03-13 19:23:52 -0300

    blog

--- single.php
+++ single.php
@@ -1,13 +1,33 @@
-<?php get_header(); ?>
+<?php 
+get_header(); 
 
-    <section class="section-default bg-light single-post">
-        <div class="container">
+$postId = $post->ID;
+$excludeID[] = $postId; 
+
+$terms = get_the_terms($post->id, 'category');
+$categoryName = $terms[0]->name;
+$categorySlug = $terms[0]->slug;
+$categoryUrl = $terms[0]->url;
+?>
+
+<div style="background-image:url('<?php the_post_thumbnail_url(); ?>');width:100%;height:540px;background-size:cover;"></div>
+
+    <section class="single-post">
+        <div class="container text bg-light">
             <div class="row section-title">
-                <div class="col-12 col-md-10 offset-md-1">
-                    <img src="<?php the_post_thumbnail_url(); ?>" class="mb-lg-5 w-100" alt="<?php the_title(); ?>">
+                <div class="col-12">                    
                     <h2><?php the_title(); ?></h2>
-                    <small><b>Publicado em:</b> <?php echo get_the_date( 'j \d\e F \d\e Y' ); ?></small>
-                    <hr />
+                    <div class="row">
+                        <div class="col-12 col-md-6">
+                            <small><b>Publicado em:</b> <?php echo get_the_date( 'j \d\e F \d\e Y' ); ?></small>
+                            <small class="d-inline-block mx-3">
+                                <b>Categoria:</b> <?php echo $categoryName; ?>
+                            </small>
+                        </div>
+                    </div>   
+                    <hr />                 
+                </div>                
+                <div class="col-12">
                     <div class="single-content">
                         <?php the_content(); ?>
                     </div>
@@ -18,23 +38,42 @@
                     <a href="<?php bloginfo('url'); ?>/blog" class="btn btn-primary">VOLTAR AO BLOG</a>
                 </div>
             </div>
-        </div>
+        </div>        
     </section>
 
-    <?php if( have_rows('big_number','option') ) { ?>
-        <section class="section-default section-bignumbers bg-col" id="<?php the_field('bignumbers_id','option'); ?>" style="background-image:url('<?php the_field('bignumbers_bg_img','option'); ?>');background-color:<?php the_field('bignumbers_bg_color','option'); ?>">
+    <?php $args = array(
+        'post_type' => 'post',
+        'post__not_in' => $excludeID,
+        'post_status' => 'publish',
+        'category_name' => $categorySlug,
+        'posts_per_page' => 3   
+    );
+    $post_query = new WP_Query($args); ?>
+
+    <?php if($post_query->have_posts() ) { ?>
+        <section class="section-default bg-light border-top">
             <div class="container">
-                <div class="row text-center">
-                    <?php while ( have_rows('big_number','option') ) : the_row(); ?>
-                        <div class="col-12 col-md">
-                            <h2 style="color:<?php the_field('bignumbers_text_color','option'); ?>"><?php the_sub_field('bignumber_number','option'); ?></h2>
-                            <p style="color:<?php the_field('bignumbers_text_color','option'); ?>"><?php the_sub_field('bignumber_description','option'); ?></p>                                
+                <div class="row section-title">
+                    <div class="col-12">
+                        <b class="subtitle">Fique por dentro</b>
+                        <h2>Posts relacionados</h2>
+                    </div>
+                </div>
+                <div class="row">                    
+                    <?php while($post_query->have_posts() ) {
+                        $post_query->the_post(); ?>
+
+                        <div class="col-12 col-md-4 mb-lg-5 wow animate__animated animate__fadeInUp">
+                            <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
+                            <small class="d-block my-2"><?php echo get_the_date( 'j \d\e F \d\e Y' ); ?></small>
+                            <h3><?php the_title(); ?></h3>
+                            <p><?php the_excerpt(); ?></p>
+                            <a href="<?php the_permalink(); ?>" class="btn-primary btn-md">LER MAIS</a>
                         </div>
-                    <?php endwhile; ?>                    
+                    <?php } ?>
                 </div>
             </div>
         </section>
     <?php } ?>
-
 
 <?php get_footer(); ?>
