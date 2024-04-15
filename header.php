df1a01b2006eb04094727ffae8e56cc78cf807f0
Matheus Decleve <me@matheus.in>
2024-04-15 10:02:34 -0300

    add facebook pixel

--- header.php
+++ header.php
@@ -31,6 +31,20 @@
     <meta property="og:image:height" content="315" />
     <meta property="og:image:type" content="image/png" />
 
+    <!-- Meta Pixel Code -->
+    <script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '1833877720412350');
+    fbq('track', 'PageView');
+    </script>
+
     <?php wp_head(); ?>
 
     <style>
@@ -45,5 +59,9 @@
     <?php the_field('code_header','option'); ?>
 </head>
 <body>
+
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1833877720412350&ev=PageView&noscript=1"
+/></noscript>
 
 <div id="bg-noise"></div>   
